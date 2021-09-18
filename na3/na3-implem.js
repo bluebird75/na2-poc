fcc919a48bffae4a3c23fc4dcd2cea36dc013396
Philippe F <phil.fremy@free.fr>
2021-09-18 09:00:39 +0200

    display randomized elements

--- na3/na3-implem.js
+++ na3/na3-implem.js
@@ -6,6 +6,20 @@ const SPT_HEIGHT = 30
 const TOP_ROW_Y = 120 - 2.5*SPT_HEIGHT
 const STACK_Y   = 118
 
+const ALL_ELT_NAMES = [
+    'na3-assets/elt0.png',
+    'na3-assets/elt1.png',
+    'na3-assets/elt2.png',
+    'na3-assets/elt3.png',
+    'na3-assets/elt4.png',
+    'na3-assets/elt5.png',
+    'na3-assets/elt6.png',
+    'na3-assets/elt7.png',
+    'na3-assets/elt12.png',
+    'na3-assets/elt17.png',
+    'na3-assets/elt18.png',
+]
+
 function na3_run() {
     console.log("Let's rock baby!") 
 
@@ -22,11 +36,14 @@ function na3_run() {
     app.stage.addChild(bg)
 
 
-    const texture = PIXI.Texture.from('na3-assets/elt6.png')
+    let textures = [];
+    ALL_ELT_NAMES.forEach((e) => {
+        textures.push( PIXI.Texture.from(e) );
+    })
 
     // top row
     for (let x = 0; x < 6; x++) {
-        let sp = new PIXI.Sprite(texture);
+        let sp = new PIXI.Sprite(textures[Math.floor(Math.random()*textures.length)]);
         sp.x = x*SPT_WIDTH
         sp.y = TOP_ROW_Y
         app.stage.addChild(sp)
@@ -35,7 +52,7 @@ function na3_run() {
     // content
     for (let x = 0; x < 6; x++) {
         for (let y = 0; y < 7; y++) {
-            let sp = new PIXI.Sprite(texture);
+            let sp = new PIXI.Sprite(textures[Math.floor(Math.random()*textures.length)]);
             sp.x = x*SPT_WIDTH
             sp.y = STACK_Y + y*SPT_HEIGHT
             app.stage.addChild(sp)
